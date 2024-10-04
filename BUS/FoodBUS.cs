f1ccf1a78222ac5f766d6771928359cf9f3c0f95
rhino-22 <trimaiqbt@gmail.com>
2024-10-05 00:26:37 +0700

    feat: add foodbus

---
+++ BUS/FoodBUS.cs
@@ -0,0 +1,17 @@
+﻿namespace CafeShop.BUS;
+
+public class FoodBUS
+{
+    private static FoodBUS _instance;
+    public static FoodBUS Instance
+    {
+        get
+        {
+            _instance ??= new FoodBUS();
+            return _instance;
+        }
+        private set { _instance = value; }
+    }
+    
+    public List<FoodDTO> GetListFood() => FoodDAO.Instance.GetListFood();
+}
